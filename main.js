525858f0229ffec177b797e9b58217a5cadca9ca
itSarthak <sarthakt3006@gmail.com>
2024-08-11 16:05:21 +0530

    Image Shink functionality added

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const os = require("os");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const slash = require("slash");
 
 // Set Environment
 process.env.NODE_ENV = "development";
@@ -106,6 +112,28 @@ const menu = [
       ]
     : []),
 ];
+
+ipcMain.on("image:minimize", (e, options) => {
+  options.dest = path.join(os.homedir(), "imageshrink"); // imageshrink is the folder created when image is compressed
+  shrinkImage(options);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg(quality),
+        imageminPngquant([pngQuality, pngQuality]),
+      ],
+    });
+    shell.openPath(dest);
+    console.log(files);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) {
